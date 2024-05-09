34b566c490107ecc839f96e304503ed1f6861de2
Shikuku39 <enshikuku39@gmail.com>
2024-05-09 08:22:25 +0300

    refactor: Update database connection configuration to use environment variables

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(session({
 dotenv.config()
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'calafiles'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 const profilepicture = multer.diskStorage({

98a87ce747b9778fd81a42ad7a868a3cfd217f44
Shikuku39 <enshikuku39@gmail.com>
2024-05-09 09:33:19 +0300

    refactor: Update app.js to use a hardcoded secret for session

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(session({
-    secret: process.env.SECRET,
+    secret: 'da',
     saveUninitialized: false,
     resave: true
 }))

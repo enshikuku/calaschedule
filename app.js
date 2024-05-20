29a4c5fff9dc186b5aa6c001b1e7ad77a124f108
Shikuku39 <enshikuku39@gmail.com>
2024-05-20 12:23:36 +0300

    refactor: Update navigation links, form validation, and components

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const connection = mysql.createConnection({
     database: process.env.DATABASE
 })
 
+// Define multer storage for profile pictures
 const profilepicture = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/profilepicture')
@@ -39,6 +40,7 @@ const profilepicture = multer.diskStorage({
 
 const uploadProfilePicture = multer({ storage: profilepicture })
 
+// Middleware to set locals
 app.use((req, res, next) => {
     res.locals.username = req.session.username !== undefined ? req.session.username : 'Guest'
     res.locals.isLoggedIn = req.session.userID !== undefined
@@ -47,38 +49,41 @@ app.use((req, res, next) => {
     next()
 })
 
-function loginRequired(req, res) {
-    res.locals.isLoggedIn || res.redirect('/login')
-}
-
+// Function to generate random id
 function generateid() {
     const characters = '0123456789'
     const genid = Array.from({ length: 3 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
     return genid.join('')
 }
 
+// Function to generate random session
 function generatesession() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     const genses = Array.from({ length: 10 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
     return genses.join('')
 }
 
+// Function to generate random OTP
 function generateotp() {
     const characters = '1234567890'
     const OTP = Array.from({ length: 4 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
     return OTP.join('')
 }
 
+// Function to check if ID exists in database
 function checkIfIdExists(id, sql) {
-    connection.query(sql, [id], (error, results) => {
-        if (results.length > 0) {
-            return true
-        } else {
-            return false
-        }
+    return new Promise((resolve, reject) => {
+        connection.query(sql, [id], (error, results) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(results.length > 0)
+            }
+        })
     })
 }
 
+// Configuration for email sending
 const config = {
     service: 'gmail',
     host: 'smtp.gmail.com',
@@ -90,45 +95,38 @@ const config = {
     }
 }
 
-const sendMail = (data) => {
+// Function to send email
+const sendMail = (data, callback) => {
     const transporter = nodemailer.createTransport(config)
     transporter.sendMail(data, (error, info) => {
         if (error) {
-            console.log(error)
+            console.error('Error sending email:', error)
+            callback(error)
         } else {
             console.log('Email sent: ' + info.response)
+            callback(null)
         }
     })
 }
 
+// Route to render the home page
 app.get('/', (req, res) => {
-    if (req.session.modesession) {
-        res.render('index')
-        return
-    }
-    let newId = ''
-    let sql = 'SELECT * FROM sessions WHERE sessionid = ?'
-    do {
-        newId = generatesession()
-    } while (checkIfIdExists(newId, sql))
-    req.session.modesession = newId
-    let sqlsession = 'INSERT INTO sessions (sessionid) VALUES (?)'
-    connection.query(sqlsession, [newId], (error, results) => {
-        res.render('index')
-    })
+    res.render('index')
 })
 
+// Route to handle signup page rendering
 app.get('/signup', (req, res) => {
     const user = {
-        name: 'Sharon Oyakhilome',
-        email: 'sharon@gmail',
-        password: '21',
-        confirmpassword: '21'
+        name: '',
+        email: '',
+        password: '',
+        confirmpassword: ''
     }
     res.render('signup', { error: false, user: user })
 })
 
-app.post('/signup', async(req, res) => {
+// Route to handle signup form submission
+app.post('/signup', async (req, res) => {
     const user = {
         name: req.body.name,
         email: req.body.email,
@@ -136,8 +134,9 @@ app.post('/signup', async(req, res) => {
         confirmpassword: req.body.confirmpassword
     }
     if (user.password === user.confirmpassword) {
-        let sql = 'SELECT * FROM user WHERE email = ?'
-        connection.query(sql, [user.email], (error, results) => {
+        try {
+            let sql = 'SELECT * FROM user WHERE email = ?'
+            const results = await checkIfIdExists(user.email, sql)
             if (results.length > 0) {
                 let message = 'An account exists with that email number!'
                 user.email = ''
@@ -147,35 +146,41 @@ app.post('/signup', async(req, res) => {
                 let sql = 'SELECT * FROM user WHERE user_id = ?'
                 do {
                     newId = generateid()
-                } while (checkIfIdExists(newId, sql))
+                } while (await checkIfIdExists(newId, sql))
 
                 let OTP = generateotp()
                 let otpsql = 'SELECT * FROM otp WHERE otpcode = ?'
                 do {
                     OTP = generateotp()
-                } while (checkIfIdExists(OTP, otpsql))
+                } while (await checkIfIdExists(OTP, otpsql))
                 req.session.otp = OTP
                 let sqlotp3 = 'SELECT * FROM otp WHERE email = ?'
-                connection.query(sqlotp3, [user.email], (error, results) => {
-                    if (results.length > 0) {
-                        let message = 'An OTP has already been sent to your email. Please check your email and enter the OTP to verify your account.'
-                        res.render('otp', { error: true, message: message, user: user, newId: newId})
-                    } else {
-                        let otpsql2 = 'INSERT INTO otp (otpcode, email) VALUES (?, ?)'
-
-                        connection.query(otpsql2, [OTP, user.email], (error, results) => {
-                            if (error) {
-                                console.error('Error inserting OTP:', error)
-                            } else {
-                                const mailData = {
-                                    from: process.env.EMAIL,
-                                    to: user.email,
-                                    subject: 'Welcome to CALAFILES - Your Personalized Learning Platform',
-                                    text: `Dear ${user.name},\n\nWelcome to CALAFILES! Your One-Time Password (OTP) for account verification is ${OTP}. This OTP will expire in 10 minutes.\n\nHappy learning!\n\nBest regards,\nThe CALAFILES Team`
+                const otpResults = await checkIfIdExists(user.email, sqlotp3)
+                if (otpResults.length > 0) {
+                    let message = 'An OTP has already been sent to your email. Please check your email and enter the OTP to verify your account.'
+                    res.render('otp', { error: true, message: message, user: user, newId: newId })
+                } else {
+                    let otpsql2 = 'INSERT INTO otp (otpcode, email) VALUES (?, ?)'
+                    connection.query(otpsql2, [OTP, user.email], async (error, results) => {
+                        if (error) {
+                            console.error('Error inserting OTP:', error)
+                            res.status(500).send('Error inserting OTP')
+                            return
+                        } else {
+                            const mailData = {
+                                from: process.env.EMAIL,
+                                to: user.email,
+                                subject: 'Welcome to CALAFILES - Your Personalized Learning Platform',
+                                text: `Dear ${user.name},\n\nWelcome to CALAFILES! Your One-Time Password (OTP) for account verification is ${OTP}. This OTP will expire in 10 minutes.\n\nHappy learning!\n\nBest regards,\nThe CALAFILES Team`
+                            }
+                            sendMail(mailData, (error) => {
+                                if (error) {
+                                    console.error('Error sending email:', error)
+                                    res.status(500).send('Error sending email')
+                                    return
                                 }
-                                sendMail(mailData)
                                 console.log('OTP inserted')
-                                res.render('otp', { error: false, user: user, newId: newId})
+                                res.render('otp', { error: false, user: user, newId: newId })
                                 setTimeout(() => {
                                     let sql = 'SELECT otpcode FROM otp WHERE email = ?'
                                     connection.query(sql, [user.email], (error, results) => {
@@ -193,19 +198,23 @@ app.post('/signup', async(req, res) => {
                                         }
                                     })
                                 }, 10 * 60 * 1000)
-                            }
-                        })
-                    }
-                })
+                            })
+                        }
+                    })
+                }
             }
-        })
+        } catch (error) {
+            console.error('Error in signup:', error)
+            res.status(500).send('Error in signup')
+        }
     } else {
-        let message = 'Passwords don\'t match!'
+        let message = 'Passwords do not match!'
         user.confirmpassword = ''
         res.render('signup', { error: true, message: message, user: user })
     }
 })
 
+// Route to handle OTP verification
 app.post('/verify-otp', (req, res) => {
     const user = {
         name: req.body.name,
@@ -214,21 +223,30 @@ app.post('/verify-otp', (req, res) => {
         id: req.body.id,
         otp: req.body.otp
     }
+
     let sqlSelect = 'SELECT * FROM otp WHERE otpcode = ? AND used = "false"'
-    connection.query(sqlSelect, [user.otp], (error, results) => {
-        if (results.length > 0) {
-            let sqlInsert = 'INSERT INTO user (name, email, password, user_id) VALUES (?, ?, ?, ?)'
-            bcrypt.hash(user.password, 10, (err, hash) => {
-                connection.query(sqlInsert, [user.name, user.email, hash, user.id], (error, results) => {
+    connection.query(sqlSelect, [user.otp], async (error, results) => {
+        try {
+            if (error) {
+                console.error('Error verifying OTP:', error)
+                res.status(500).send('Error verifying OTP')
+                return
+            }
+            if (results.length > 0) {
+                let sqlInsert = 'INSERT INTO user (name, email, password, user_id) VALUES (?, ?, ?, ?)'
+                const hash = await bcrypt.hash(user.password, 10)
+                connection.query(sqlInsert, [user.name, user.email, hash, user.id], async (error, results) => {
                     if (error) {
                         console.error('Error inserting user:', error)
-                        return res.status(500).send('Error inserting user')
+                        res.status(500).send('Error inserting user')
+                        return
                     }
                     let sqlUpdate = 'UPDATE otp SET used = "true" WHERE otpcode = ?'
-                    connection.query(sqlUpdate, [user.otp], (error, results) => {
+                    connection.query(sqlUpdate, [user.otp], async (error, results) => {
                         if (error) {
                             console.error('Error updating OTP:', error)
-                            return res.status(500).send('Error updating OTP')
+                            res.status(500).send('Error updating OTP')
+                            return
                         }
                         const mailData = {
                             from: process.env.EMAIL,
@@ -236,20 +254,30 @@ app.post('/verify-otp', (req, res) => {
                             subject: 'Welcome to CALAFILES - Your Personalized Learning Platform',
                             text: `Dear ${user.name},\n\nWelcome to CALAFILES! Your account has been successfully created. Start your personalized learning journey today.\n\nIf you have any questions or need assistance, feel free to reach out to our support team.\n\nHappy learning!\n\nBest regards,\nThe CALAFILES Team`
                         }
-                        sendMail(mailData)
-                        req.session.userID = user.id
-                        req.session.username = user.name.split(' ')[0]
-                        res.redirect('/dashboard')
+                        sendMail(mailData, (error) => {
+                            if (error) {
+                                console.error('Error sending email:', error)
+                                res.status(500).send('Error sending email')
+                                return
+                            }
+                            req.session.userID = user.id
+                            req.session.username = user.name.split(' ')[0]
+                            res.redirect('/dashboard')
+                        })
                     })
                 })
-            })
-        } else {
-            let message = 'Invalid OTP!'
-            res.render('otp', { error: true, message: message, user: user, newId: user.id })
+            } else {
+                let message = 'Invalid OTP!'
+                res.render('otp', { error: true, message: message, user: user, newId: user.id })
+            }
+        } catch (error) {
+            console.error('Error in OTP verification:', error)
+            res.status(500).send('Error in OTP verification')
         }
     })
 })
 
+// Route to render the login page
 app.get('/login', (req, res) => {
     const user = {
         email: '',
@@ -258,38 +286,51 @@ app.get('/login', (req, res) => {
     res.render('login', { error: false, user: user })
 })
 
+// Route to handle login form submission
 app.post('/login', (req, res) => {
     const user = {
         email: req.body.email,
         password: req.body.password
     }
+
     let sql = 'SELECT * FROM user WHERE email = ?'
-    connection.query(sql, [user.email], (error, results) => {
-        if (results.length > 0) {
-            bcrypt.compare(user.password, results[0].password, (error, passwordMatches) => {
+    connection.query(sql, [user.email], async (error, results) => {
+        try {
+            if (error) {
+                console.error('Error querying user:', error)
+                res.status(500).send('Error querying user')
+                return
+            }
+            if (results.length > 0) {
+                const passwordMatches = await bcrypt.compare(user.password, results[0].password)
                 if (passwordMatches) {
-                    req.session.userID = results[0].id
+                    req.session.userID = results[0].user_id
                     req.session.username = results[0].name.split(' ')[0]
                     res.redirect('/dashboard')
                 } else {
                     let message = 'Incorrect password!'
                     res.render('login', { error: true, message: message, user: user })
                 }
-            })
-        } else {
-            let message = 'Account does not exist. Please create one'
-            res.render('login', { error: true, message: message, user: user })
+            } else {
+                let message = 'Account does not exist. Please create one'
+                res.render('login', { error: true, message: message, user: user })
+            }
+        } catch (error) {
+            console.error('Error in login:', error)
+            res.status(500).send('Error in login')
         }
     })
 })
 
 app.get('/dashboard', (req, res) => {
-    loginRequired(req, res)
-    res.render('dashboard')
+    if (req.session.userID) {
+        res.render('dashboard')
+    } else {
+        res.redirect('/login')
+    }
 })
 
 app.get('/logout', (req, res) => {
-    loginRequired(req, res)
     req.session.destroy((err) => {
         res.redirect('/')
     })
